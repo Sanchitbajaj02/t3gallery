03814b82d4009cb815763bf1038565a4c9293fb4
Sanchit Bajaj <sanchit02@outlook.com>
2024-05-04 21:41:23 +0530

    implement custom button

--- src/components/core/navbar.tsx
+++ src/components/core/navbar.tsx
@@ -1,32 +1,30 @@
-"use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+// import { UploadButton } from "~/utils/uploadthing";
 import Link from "next/link";
+import SimpleUploadButton from "./simpleUploadButton";
 
 export default function TopNavbar() {
-  const router = useRouter();
-
   return (
     <nav className="flex w-full items-center justify-between border-b px-8 py-4 text-xl font-semibold">
       <Link href={"/"} className="cursor-pointer">
         Gallery
       </Link>
-      <div className="flex flex-row justify-center gap-4">
+      <div className="flex flex-row items-center justify-center gap-4">
         <SignedOut>
           <div className="rounded bg-white px-4 py-1 text-base text-black hover:bg-gray-100">
             <SignInButton />
           </div>
         </SignedOut>
         <SignedIn>
-          <UploadButton
+          {/*<UploadButton
             endpoint="imageUploader"
             onClientUploadComplete={() => {
               router.refresh();
             }}
             className="text-base"
-          />
+          />*/}
 
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
